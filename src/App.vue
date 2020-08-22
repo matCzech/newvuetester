6937e356759f091a8a3b2de9ffb6e70bf2f53af9
Mateusz Czech <czechmateusz88@gmail.com>
2020-08-22 22:49:33 +0200

    first cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <router-link to="/">Stellaris</router-link> |
+      <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style lang="scss" scoped>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
